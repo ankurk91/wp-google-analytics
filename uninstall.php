a3eca49b03807ba218dfd3eaf473ac7d1aa24dfb
ank91 <pro.ankurk1@gmail.com>
2015-04-26 17:20:50 +0530

    Updated to v0.3

--- uninstall.php
+++ uninstall.php
@@ -5,12 +5,31 @@
 *   This file will be used to remove all traces of this plugin when uninstalled
 */
 
-//if uninstall not called by WordPress do exit
+// Make sure that we are uninstalling
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
+    exit();
 
-if (!defined('ABSPATH') && !defined('WP_UNINSTALL_PLUGIN'))
-    exit;
 
+$option_name = 'asga_options' ;
 /*
  * lets remove the database entry created by this plugin
  */
-delete_option('asga_options');
+
+if ( !is_multisite() )
+{
+    delete_option( $option_name );
+}
+else
+{
+    global $wpdb;
+    $blog_ids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs");
+    $original_blog_id = get_current_blog_id();
+
+    foreach ( $blog_ids as $blog_id )
+    {
+        switch_to_blog( $blog_id );
+        delete_option( $option_name );
+
+    }
+    switch_to_blog( $original_blog_id );
+}
