ea29ffb6aa4541fafb5d90749fac2ae40d6bf50c
Ankur Kumar <pro.ankurk1@gmail.com>
2015-06-16 11:29:51 +0530

    updated to 0.5

--- ank-simplified-ga.php
+++ ank-simplified-ga.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: Ank Simplified GA
+Plugin Name: Ank Simplified Google Analytics
 Plugin URI: https://github.com/ank91/ank-simplified-ga
 Description: Simple, light weight, and non-bloated WordPress Google Analytics Plugin.
-Version: 0.4
+Version: 0.5
 Author: Ankur Kumar
 Author URI: http://ank91.github.io/
 License: GPL2
@@ -14,7 +14,7 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 /* No direct access*/
 if (!defined('ABSPATH')) exit;
 
-define('ASGA_PLUGIN_VER', '0.4');
+define('ASGA_PLUGIN_VER', '0.5');
 define('ASGA_BASE_FILE', __FILE__);
 
 class Ank_Simplified_GA
@@ -75,8 +75,9 @@ class Ank_Simplified_GA
         global $wp_query;
         //check for debug mode
         $debug_mode = $this->check_debug_mode($options);
-        //flag
+        //this flag will be used in view
         $user_engagement = absint($options['log_user_engagement']);
+        $js_load_later = absint($options['js_load_later']);
 
 
         if ($options['ua_enabled'] == 1) {
@@ -126,6 +127,7 @@ class Ank_Simplified_GA
                 }
             }
             $gaq[] = "'_setAccount', '" . esc_attr($ga_id) . "'";
+
             if ($domain !== 'auto') {
                 $gaq[] = "'_setDomainName', '" . esc_attr($domain) . "'";
             }
@@ -134,10 +136,10 @@ class Ank_Simplified_GA
                 $gaq[] = "'_gat._forceSSL'";
             }
 
-
             if ($options['anonymise_ip'] == 1) {
                 $gaq[] = "'_gat._anonymizeIp'";
             }
+
             $ela_plugin_url = '';
             if ($options['ga_ela'] == 1) {
                 $ela_plugin_url = "var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';\n";
@@ -164,9 +166,8 @@ class Ank_Simplified_GA
      */
     private function check_debug_mode($options)
     {
-        //debug mode is for admins only
-        //todo check for multi-site admin as well
-        if (current_user_can('manage_options')) {
+        //debug mode is for logged-in admins/network admins only
+        if (current_user_can('manage_options') || is_super_admin()) {
             if ($options['debug_mode'] == 1) {
                 return true;
             }
@@ -221,8 +222,8 @@ if (is_admin()) {
     /* Load admin part only if we are inside wp-admin */
     require(trailingslashit(dirname(__FILE__)) . "asga-admin.php");
     //init admin class
-    global $ASGA_Admin_Class;
-    $ASGA_Admin_Class = ASGA_Admin_Class::get_instance();
+    global $Ank_Simplified_GA_Admin;
+    $Ank_Simplified_GA_Admin = Ank_Simplified_GA_Admin::get_instance();
 }
 else {
     /*init front end part*/
--- views/classic_script.php
+++ views/classic_script.php
@@ -1,18 +1,26 @@
 <!--Classic GA Tracking start<?php if($debug_mode===true){?>, Debugging is on<?php }?>-->
 <script type="text/javascript">
+<?php
+if($js_load_later===1){ ?>
+function _loadGA(){
+<?php } ?>
 var _gaq = _gaq || [];
 <?php
 echo $ela_plugin_url;
 foreach($gaq as $item){
 echo "_gaq.push([" . $item . "]);\n";
 }
-if($user_engagement==1){ ?>
-setTimeout(function(){_gaq.push(['_trackEvent','Page Read',document.location.href,document.referrer])},15E3);
+if($user_engagement===1){ ?>
+setTimeout(function(){_gaq.push(['_trackEvent','User Engagement','Read',window.location.href])},15E3);
 <?php } ?>
 (function() {
 var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 ga.src = <?php echo $ga_src; ?>;
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+<?php if($js_load_later===1) {?>
+}
+window.addEventListener?window.addEventListener("load",_loadGA,!1):window.attachEvent?window.attachEvent("onload",_loadGA):window.onload=_loadGA;
+<?php }?>
 </script>
 <!--GA Tracking ends-->
