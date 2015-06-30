7c2d9e4bbea52df7f7aa5d469d24cb39354189b7
ank91 <pro.ankurk1@gmail.com>
2015-06-30 18:56:16 +0530

    security enhancements

--- uninstall.php
+++ uninstall.php
@@ -5,34 +5,30 @@
 */
 
 // Make sure that we are uninstalling
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
+if (!defined('WP_UNINSTALL_PLUGIN'))
     exit;
 
 
-$option_name = 'asga_options' ;
+$option_name = 'asga_options';
 $transient_name = 'asga_js_cache';
 
 /*
  * lets remove the database entry(s) created by this plugin
  */
 
-if ( !is_multisite() )
-{
-    delete_option( $option_name );
+if (!is_multisite()) {
+    delete_option($option_name);
     delete_transient($transient_name);
-}
-else
-{
+} else {
     global $wpdb;
     $blog_ids = $wpdb->get_col("SELECT blog_id FROM $wpdb->blogs");
     $original_blog_id = get_current_blog_id();
 
-    foreach ( $blog_ids as $blog_id )
-    {
-        switch_to_blog( $blog_id );
-        delete_option( $option_name );
+    foreach ($blog_ids as $blog_id) {
+        switch_to_blog($blog_id);
+        delete_option($option_name);
         delete_transient($transient_name);
 
     }
-    switch_to_blog( $original_blog_id );
+    switch_to_blog($original_blog_id);
 }
