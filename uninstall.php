ea7b1f80507a65de47f0b32f5da03f3b239b3724
Ankur Kumar <pro.ankurk1@gmail.com>
2015-10-16 12:11:51 +0530

    Feature: Link to google webmaster

--- uninstall.php
+++ uninstall.php
@@ -11,24 +11,26 @@ if (!defined('WP_UNINSTALL_PLUGIN'))
     exit;
 
 
-$option_name = 'asga_options';
-
-
-/*
- * Lets remove the database entry(s) created by this plugin
+/**
+ * Remove the database entry(s) created by this plugin
+ * @param $option_name string
  */
-
-if (is_multisite() === false) {
-    delete_option($option_name);
-} else {
-    global $wpdb;
-    $blog_ids = $wpdb->get_col("SELECT blog_id FROM $wpdb->blogs");
-    $original_blog_id = get_current_blog_id();
-
-    foreach ($blog_ids as $blog_id) {
-        switch_to_blog($blog_id);
+function uninstall_ank_simplified_ga($option_name)
+{
+    if (is_multisite() === false) {
         delete_option($option_name);
+    } else {
+        global $wpdb;
+        $blog_ids = $wpdb->get_col("SELECT blog_id FROM $wpdb->blogs");
+        $original_blog_id = get_current_blog_id();
 
+        foreach ($blog_ids as $blog_id) {
+            switch_to_blog($blog_id);
+            delete_option($option_name);
+
+        }
+        switch_to_blog($original_blog_id);
     }
-    switch_to_blog($original_blog_id);
 }
+
+uninstall_ank_simplified_ga('asga_options');
