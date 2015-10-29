a0cf1d3536362218c983343fb4c81b5e3f6db987
austinankur <austin.ankur@ithands.net>
2015-10-29 16:06:53 +0530

    PreRelease 0.9.6, still need some testing

--- frontend/views/ga_disabled.php
+++ frontend/views/ga_disabled.php
@@ -1,2 +1,1 @@
-<!-- ASGA (v<?php echo ASGA_PLUGIN_VER ?>) is disabled-->
-<!-- Reason: <?php echo $options['reason'] ?>-->
+<!-- ASGA (v<?php echo ASGA_PLUGIN_VER ?>) is disabled, Reason: <?php echo $options['reason'] ?>-->
