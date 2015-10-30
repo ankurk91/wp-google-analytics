4fcf2769dfda1c24156b7756bec2e20d50d5d0de
ank91 <pro.ankurk1@gmail.com>
2015-10-30 12:43:49 +0530

    Fixed several minor bugs

--- frontend/asga-frontend.php
+++ frontend/asga-frontend.php
@@ -59,9 +59,9 @@ class Ank_Simplified_GA
 
         //decide where to print code
         if ($this->asga_options['js_location'] == 1) {
-            add_action('wp_head', array($this, 'print_tracking_code'), $js_priority);
+            add_action('wp_head', array($this, 'decide_tracking_code'), $js_priority);
         } else {
-            add_action('wp_footer', array($this, 'print_tracking_code'), $js_priority);
+            add_action('wp_footer', array($this, 'decide_tracking_code'), $js_priority);
         }
 
         //check for webmaster code
@@ -80,7 +80,7 @@ class Ank_Simplified_GA
     /**
      * Prepare and print javascript code to front end
      */
-    function print_tracking_code()
+    function decide_tracking_code()
     {
 
         //get database options
@@ -93,16 +93,15 @@ class Ank_Simplified_GA
             return;
         }
 
+        $ga_array = array();
         //get tracking id
-        $ga_id = esc_js($options['ga_id']);
+        $ga_array['id'] = esc_js($options['ga_id']);
         //decide sub-domain
-        $domain = empty($options['ga_domain']) ? 'auto' : esc_js($options['ga_domain']);
+        $ga_array['domain'] = empty($options['ga_domain']) ? 'auto' : esc_js($options['ga_domain']);
 
         //these flags will be used in view
         $view_array = array(
-            'gaq' => array(
-                'custom_trackers' => array()
-            )
+            'gaq' => array()
         );
 
         //check for debug mode
@@ -111,117 +110,141 @@ class Ank_Simplified_GA
         $view_array['js_load_later'] = absint($options['js_load_later']);
 
 
-        global $wp_query;
-
         if ($options['ua_enabled'] == 1) {
             //if universal is enabled
-
-            if ($options['allow_linker'] == 1 && $options['allow_anchor'] == 0) {
-                $view_array['gaq'][] = "'create', '" . $ga_id . "', '" . $domain . "', {'allowLinker': true}";
-            } else {
-                if ($options['allow_anchor'] == 1 && $options['allow_linker'] == 0) {
-                    $view_array['gaq'][] = "'create', '" . $ga_id . "', '" . $domain . "', {'allowAnchor': true}";
-                } else {
-                    if ($options['allow_linker'] == 1 && $options['allow_anchor'] == 1) {
-                        $view_array['gaq'][] = "'create', '" . $ga_id . "', '" . $domain . "', {'allowLinker': true,'allowAnchor': true}";
-                    } else {
-                        $view_array['gaq'][] = "'create', '" . $ga_id . "', '" . $domain . "'";
-                    }
-                }
-            }
-
-            if ($options['force_ssl'] == 1) {
-                $view_array['gaq'][] = "'set', 'forceSSL', true";
-            }
-
-            if ($options['anonymise_ip'] == 1) {
-                $view_array['gaq'][] = "'set', 'anonymizeIp', true";
-            }
-            /* Enable demographics and interests reports */
-            if ($options['displayfeatures'] == 1) {
-                $view_array['gaq'][] = "'require', 'displayfeatures'";
-            }
-            /* Enhanced Link Attribution */
-            if ($options['ga_ela'] == 1) {
-                $view_array['gaq'][] = "'require', 'linkid'";
-            }
-            if ($options['custom_trackers'] !== '') {
-                $view_array['gaq'][] = array(
-                    'custom_trackers' => $options['custom_trackers']
-                );
-            }
-
-            if (is_404() && $options['log_404'] == 1) {
-                $view_array['gaq'][] = "'send','event','404',document.location.href,document.referrer";
-            } elseif ($wp_query->is_search && $options['log_search'] == 1) {
-                $view_array['gaq'][] = "'send','pageview','/?s=" . rawurlencode($wp_query->query_vars['s']) . "'";
-            } else {
-                $view_array['gaq'][] = "'send','pageview'";
-            }
-
-            $this->load_view('universal_script.php', $view_array);
+            $this->print_universal_code($view_array, $ga_array);
 
         } else {
             //classic ga is enabled
+            $this->print_classic_code($view_array, $ga_array);
 
-            $view_array['ga_src'] = "('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'";
-            if ($view_array['debug_mode'] == true) {
-                //Did u notice additional /u in url ?
-                //@source https://developers.google.com/analytics/resources/articles/gaTrackingTroubleshooting#gaDebug
-                $view_array['ga_src'] = "('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/u/ga_debug.js'";
-            }
-            //@source https://support.google.com/analytics/answer/2444872
-            if ($options['displayfeatures'] == 1) {
-                $view_array['ga_src'] = "('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js'";
-                if ($view_array['debug_mode'] == true) {
-                    $view_array['ga_src'] = "('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc_debug.js'";
-                }
-            }
-            $view_array['gaq'][] = "'_setAccount', '" . $ga_id . "'";
-
-            if ($domain !== 'auto') {
-                $view_array['gaq'][] = "'_setDomainName', '" . $domain . "'";
-            }
-
-            if ($options['allow_linker'] == 1) {
-                $view_array['gaq'][] = "'_setAllowLinker', true";
-            }
-
-            if ($options['allow_anchor'] == 1) {
-                $view_array['gaq'][] = "'_setAllowAnchor', true";
-            }
-
-            if ($options['force_ssl'] == 1) {
-                $view_array['gaq'][] = "'_gat._forceSSL'";
-            }
-
-            if ($options['anonymise_ip'] == 1) {
-                $view_array['gaq'][] = "'_gat._anonymizeIp'";
-            }
-
-            $view_array['ela_plugin_url'] = false;
-            if ($options['ga_ela'] == 1) {
-                $view_array['ela_plugin_url'] = "var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';\n";
-                $view_array['gaq'][] = "['_require', 'inpage_linkid', pluginUrl]";
-            }
-
-            if ($options['custom_trackers'] !== '') {
-                $view_array['gaq'][] = array(
-                    'custom_trackers' => $options['custom_trackers']
-                );
-            }
-
-            if (is_404() && $options['log_404'] == 1) {
-                $view_array['gaq'][] = "'_trackEvent','404',document.location.href,document.referrer";
-            } elseif ($wp_query->is_search && $options['log_search'] == 1) {
-                $view_array['gaq'][] = "'_trackPageview','/?s=" . rawurlencode($wp_query->query_vars['s']) . "'";
-            } else {
-                $view_array['gaq'][] = "'_trackPageview'";
-            }
-
-            $this->load_view('classic_script.php', $view_array);
         }
 
+    }
+
+    /**
+     * Prepare classic tracing code and print
+     * @param $view_array array Array to be passed to view
+     * @param $ga array
+     */
+    private function print_classic_code($view_array, $ga)
+    {
+        global $wp_query;
+        $options = $this->asga_options;
+
+        $view_array['ga_src'] = "('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'";
+        if ($view_array['debug_mode'] == true) {
+            //Did u notice additional /u in url ?
+            //@source https://developers.google.com/analytics/resources/articles/gaTrackingTroubleshooting#gaDebug
+            $view_array['ga_src'] = "('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/u/ga_debug.js'";
+        }
+        //@source https://support.google.com/analytics/answer/2444872
+        if ($options['displayfeatures'] == 1) {
+            $view_array['ga_src'] = "('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js'";
+            if ($view_array['debug_mode'] == true) {
+                $view_array['ga_src'] = "('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc_debug.js'";
+            }
+        }
+        $view_array['gaq'][] = "'_setAccount', '" . $ga['id'] . "'";
+
+        if ($ga['domain'] !== 'auto') {
+            $view_array['gaq'][] = "'_setDomainName', '" . $ga['domain'] . "'";
+        }
+
+        if ($options['allow_linker'] == 1) {
+            $view_array['gaq'][] = "'_setAllowLinker', true";
+        }
+
+        if ($options['allow_anchor'] == 1) {
+            $view_array['gaq'][] = "'_setAllowAnchor', true";
+        }
+
+        if ($options['force_ssl'] == 1) {
+            $view_array['gaq'][] = "'_gat._forceSSL'";
+        }
+
+        if ($options['anonymise_ip'] == 1) {
+            $view_array['gaq'][] = "'_gat._anonymizeIp'";
+        }
+
+        $view_array['ela_plugin_url'] = false;
+        if ($options['ga_ela'] == 1) {
+            $view_array['ela_plugin_url'] = "var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';\n";
+            $view_array['gaq'][] = "['_require', 'inpage_linkid', pluginUrl]";
+        }
+
+        if ($options['custom_trackers'] !== '') {
+            $view_array['gaq'][] = array(
+                'custom_trackers' => $options['custom_trackers']
+            );
+        }
+
+        if (is_404() && $options['log_404'] == 1) {
+            $view_array['gaq'][] = "'_trackEvent','404',document.location.href,document.referrer";
+        } elseif ($wp_query->is_search && $options['log_search'] == 1) {
+            $view_array['gaq'][] = "'_trackPageview','/?s=" . rawurlencode($wp_query->query_vars['s']) . "'";
+        } else {
+            $view_array['gaq'][] = "'_trackPageview'";
+        }
+
+        $this->load_view('classic_script.php', $view_array);
+
+    }
+
+    /**
+     * Prepare universal tracking code and print
+     * @param $view_array array Array to be passed to view
+     * @param $ga array
+     */
+    private function print_universal_code($view_array, $ga)
+    {
+        global $wp_query;
+        $options = $this->asga_options;
+
+        if ($options['allow_linker'] == 1 && $options['allow_anchor'] == 0) {
+            $view_array['gaq'][] = "'create', '" . $ga['id'] . "', '" . $ga['domain'] . "', {'allowLinker': true}";
+        } else {
+            if ($options['allow_anchor'] == 1 && $options['allow_linker'] == 0) {
+                $view_array['gaq'][] = "'create', '" . $ga['id'] . "', '" . $ga['domain'] . "', {'allowAnchor': true}";
+            } else {
+                if ($options['allow_linker'] == 1 && $options['allow_anchor'] == 1) {
+                    $view_array['gaq'][] = "'create', '" . $ga['id'] . "', '" . $ga['domain'] . "', {'allowLinker': true,'allowAnchor': true}";
+                } else {
+                    $view_array['gaq'][] = "'create', '" . $ga['id'] . "', '" . $ga['domain'] . "'";
+                }
+            }
+        }
+
+        if ($options['force_ssl'] == 1) {
+            $view_array['gaq'][] = "'set', 'forceSSL', true";
+        }
+
+        if ($options['anonymise_ip'] == 1) {
+            $view_array['gaq'][] = "'set', 'anonymizeIp', true";
+        }
+        /* Enable demographics and interests reports */
+        if ($options['displayfeatures'] == 1) {
+            $view_array['gaq'][] = "'require', 'displayfeatures'";
+        }
+        /* Enhanced Link Attribution */
+        if ($options['ga_ela'] == 1) {
+            $view_array['gaq'][] = "'require', 'linkid'";
+        }
+        if ($options['custom_trackers'] !== '') {
+            $view_array['gaq'][] = array(
+                'custom_trackers' => $options['custom_trackers']
+            );
+        }
+
+        if (is_404() && $options['log_404'] == 1) {
+            $view_array['gaq'][] = "'send','event','404',document.location.href,document.referrer";
+        } elseif ($wp_query->is_search && $options['log_search'] == 1) {
+            $view_array['gaq'][] = "'send','pageview','/?s=" . rawurlencode($wp_query->query_vars['s']) . "'";
+        } else {
+            $view_array['gaq'][] = "'send','pageview'";
+        }
+
+        $this->load_view('universal_script.php', $view_array);
     }
 
     /**
@@ -242,9 +265,9 @@ class Ank_Simplified_GA
         if ($this->is_tracking_possible() === true) {
             $is_min = (WP_DEBUG == 1) ? '' : '.min';
             //depends on jquery
-            wp_enqueue_script('asga-event-log', plugins_url('/js/event-tracking' . $is_min . '.js', __FILE__), array('jquery'), ASGA_PLUGIN_VER, true);
+            wp_enqueue_script('asga-event-tracking', plugins_url('/js/event-tracking' . $is_min . '.js', __FILE__), array('jquery'), ASGA_PLUGIN_VER, true);
             //wp inbuilt hack to print js options object just before this script
-            wp_localize_script('asga-event-log', 'asga_opt', $this->get_js_options());
+            wp_localize_script('asga-event-tracking', 'asga_opt', $this->get_js_options());
         }
     }
 
@@ -283,7 +306,7 @@ class Ank_Simplified_GA
 
     /**
      * Function determines whether to print tracking code or not
-     * @return mixed true if possible and string if not possible
+     * @return mixed Return true if possible and string if not possible
      */
     private function is_tracking_possible()
     {
