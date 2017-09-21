5136eb6c77ca048c696d3577ad6c1a2699dfe902
ankurk91 <pro.ankurk1@gmail.com>
2017-09-21 10:55:43 +0530

    Send 1 as value in events

--- assets/front-end.js
+++ assets/front-end.js
@@ -1,98 +1,101 @@
 (function (window, document) {
-    'use strict';
+  'use strict';
 
-    // Get dynamic options from page
-    var asgaOpt = window._asgaOpt;
+  // Get dynamic options from page
+  var asgaOpt = window._asgaOpt;
 
-    document.addEventListener("DOMContentLoaded", function (event) {
+  document.addEventListener("DOMContentLoaded", function (event) {
 
-        // Track Downloads
-        if (asgaOpt.downloadLinks === '1') {
-            // https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions
-            var exts = (asgaOpt.downloadExt === '') ? 'doc*|xls*|ppt*|pdf|zip|rar|exe|mp3' : asgaOpt.downloadExt.replace(/,/g, '|');
+	// Track Downloads
+	if (asgaOpt.downloadLinks === '1') {
+	  // https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions
+	  var exts = (asgaOpt.downloadExt === '') ? 'doc*|xls*|ppt*|pdf|zip|rar|mp3' : asgaOpt.downloadExt.replace(/,/g, '|');
 
-            // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/RegExp
-            var regExt = new RegExp(".*\\.(" + exts + ")(\\?.*)?$");
+	  // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/RegExp
+	  var regExt = new RegExp(".*\\.(" + exts + ")(\\?.*)?$");
 
-            var downLinks = document.querySelectorAll('a');
+	  var downLinks = document.querySelectorAll('a');
 
-            Array.prototype.forEach.call(downLinks, function (link) {
-                // Include only internal links for downloads
-                if (link.hostname && (link.hostname === window.location.hostname) && link.href.match(regExt)) {
-                    link.addEventListener('click', function (e) {
-                        logClickEvent('Downloads', this.href, e)
-                    });
+	  Array.prototype.forEach.call(downLinks, function (link) {
+		// Include only internal links for downloads
+		if (link.hostname && (link.hostname === window.location.hostname) && link.href.match(regExt)) {
+		  link.addEventListener('click', function (e) {
+			logClickEvent('Downloads', this.href, e)
+		  });
 
-                    // Only add download attribute if does not have
-                    if (!link.hasAttribute('download'))
-                        link.setAttribute('download', '');
+		  // Only add download attribute if does not have
+		  if (!link.hasAttribute('download'))
+			link.setAttribute('download', '');
 
-                }
-            });
+		}
+	  });
 
-        }
+	}
 
-        // Track Mailto links
-        if (asgaOpt.mailLinks === '1') {
-            var mailLinks = document.querySelectorAll('a[href^="mailto"]');
+	// Track Mailto links
+	if (asgaOpt.mailLinks === '1') {
+	  var mailLinks = document.querySelectorAll('a[href^="mailto"]');
 
-            Array.prototype.forEach.call(mailLinks, function (link) {
-                link.addEventListener('click', function (e) {
-                    // Label should not include 'mailto'
-                    logClickEvent('Email', this.href.replace(/^mailto\:/i, '').toLowerCase(), e)
-                })
-            });
+	  Array.prototype.forEach.call(mailLinks, function (link) {
+		link.addEventListener('click', function (e) {
+		  // Label should not include 'mailto'
+		  logClickEvent('Email', this.href.replace(/^mailto\:/i, '').toLowerCase(), e)
+		})
+	  });
 
-        }
+	}
 
-        // Track Outbound Links
-        if (asgaOpt.outgoingLinks === '1') {
-            var outLinks = document.querySelectorAll('a[href^="http"]');
+	// Track Outbound Links
+	if (asgaOpt.outgoingLinks === '1') {
+	  var outLinks = document.querySelectorAll('a[href^="http"]');
 
-            Array.prototype.forEach.call(outLinks, function (link) {
-                // https://css-tricks.com/snippets/jquery/target-only-external-links/
-                if (link.hostname && link.hostname !== window.location.hostname) {
-                    link.addEventListener('click', function (e) {
-                        logClickEvent('Outbound', (asgaOpt.outboundLinkType === '1') ? this.hostname : this.href, e);
-                    });
-                    link.setAttribute('target', '_blank'); // make sure these links open in new tab
+	  Array.prototype.forEach.call(outLinks, function (link) {
+		// https://css-tricks.com/snippets/jquery/target-only-external-links/
+		if (link.hostname && link.hostname !== window.location.hostname) {
+		  link.addEventListener('click', function (e) {
+			logClickEvent('Outbound', (asgaOpt.outboundLinkType === '1') ? this.hostname : this.href, e);
+		  });
+		  link.setAttribute('target', '_blank'); // make sure these links open in new tab
 
-                }
+		}
 
-            });
+	  });
 
-        }
+	}
 
-    });
+  });
 
 
-    /**
-     * Detect Analytics type and send event
-     * @ref https://support.google.com/analytics/answer/1033068
-     * @param category string
-     * @param label string
-     * @param event click event
-     */
-    function logClickEvent(category, label, event) {
-        // return early if event.preventDefault() was ever called on this event object.
-        if (event.defaultPrevented) return;
+  /**
+   * Detect Analytics type and send event
+   * @ref https://support.google.com/analytics/answer/1033068
+   * @param category string
+   * @param label string
+   * @param event click event
+   */
+  function logClickEvent(category, label, event) {
+	// Return early if event.preventDefault() was ever called on this event object.
+	if (event.defaultPrevented) return;
 
-        // if label is not set then exit
-        if (typeof label === 'undefined' || label === '') return;
+	// If label is not set then exit
+	if (typeof label === 'undefined' || label === '') return;
+	var nonInteractive = (asgaOpt.nonInteractive === '1');
 
-        if (window.ga && ga.hasOwnProperty('loaded') && ga.loaded === true && ga.create) {
-            // Universal event tracking
-            // https://developers.google.com/analytics/devguides/collection/analyticsjs/events
-            ga('send', 'event', category, 'click', label, {
-                nonInteraction: (asgaOpt.nonInteractive === '1')
-            });
-        } else if (window._gaq && _gaq._getAsyncTracker) {
-            // Classic event tracking
-            // https://developers.google.com/analytics/devguides/collection/gajs/eventTrackerGuide
-            _gaq.push(['_trackEvent', category, 'click', label, 1, (asgaOpt.nonInteractive === '1')]);
-        } else {
-            (window.console) ? console.info('Google analytics not loaded yet.') : null
-        }
-    }
+	if (window.ga && ga.hasOwnProperty('loaded') && ga.loaded === true && ga.create) {
+	  // Universal event tracking
+	  // https://developers.google.com/analytics/devguides/collection/analyticsjs/events
+	  ga('send', 'event', category, 'click', label, 1,
+		  {
+			nonInteraction: nonInteractive
+		  }
+	  );
+	} else if (window._gaq && _gaq._getAsyncTracker) {
+	  // Classic event tracking
+	  // https://developers.google.com/analytics/devguides/collection/gajs/eventTrackerGuide
+	  _gaq.push(['_trackEvent', category, 'click', label, 1, nonInteractive]);
+	} else {
+	  (window.console) ? console.info('Google analytics not loaded yet.') : null
+	}
+  }
 
 })(window, document);
--- assets/option-page.js
+++ assets/option-page.js
@@ -1,47 +1,47 @@
 (function (window, jQuery) {
-    'use strict';
-    // Get requested tab from url
-    var requestedTab = window.location.hash.replace('#top#', '');
+  'use strict';
+  // Get requested tab from url
+  var requestedTab = window.location.hash.replace('#top#', '');
 
-    jQuery(function ($) {
-        /**
-         * Cache DOM elements for later use
-         */
-        var $gaTabs = $('h2#ga-tabs'),
-            $input = $("form#asga_form").find('input:hidden[name="_wp_http_referer"]'),
-            $sections = $('section.tab-content');
+  jQuery(function ($) {
+	/**
+	 * Cache DOM elements for later use
+	 */
+	var $gaTabs = $('h2#ga-tabs'),
+		$input = $("form#asga_form").find('input:hidden[name="_wp_http_referer"]'),
+		$sections = $('section.tab-content');
 
-        // If there no active tab found , set first tab as active
-        if (requestedTab === '' || $('#' + requestedTab).length === 0) requestedTab = $sections.attr('id');
-        // Notice: we are not using cached DOM in next line
-        $('#' + requestedTab).addClass('active');
-        $('#' + requestedTab + '-tab').addClass('nav-tab-active');
-        // Set return tab on page load
-        setRedirectURL(requestedTab);
+	// If there no active tab found , set first tab as active
+	if (requestedTab === '' || $('#' + requestedTab).length === 0) requestedTab = $sections.attr('id');
+	// Notice: we are not using cached DOM in next line
+	$('#' + requestedTab).addClass('active');
+	$('#' + requestedTab + '-tab').addClass('nav-tab-active');
+	// Set return tab on page load
+	setRedirectURL(requestedTab);
 
-        // Bind a click event to all tabs
-        $gaTabs.find('a.nav-tab').on('click.asga', (function (e) {
-            e.stopPropagation();
-            // Hide all tabs
-            $gaTabs.find('a.nav-tab').removeClass('nav-tab-active');
-            $sections.removeClass('active');
-            // Activate only clicked tab
-            var id = $(this).attr('id').replace('-tab', '');
-            $('#' + id).addClass('active');
-            $(this).addClass('nav-tab-active');
-            // Set return tab url
-            setRedirectURL(id);
-        }));
+	// Bind a click event to all tabs
+	$gaTabs.find('a.nav-tab').on('click.asga', (function (e) {
+	  e.stopPropagation();
+	  // Hide all tabs
+	  $gaTabs.find('a.nav-tab').removeClass('nav-tab-active');
+	  $sections.removeClass('active');
+	  // Activate only clicked tab
+	  var id = $(this).attr('id').replace('-tab', '');
+	  $('#' + id).addClass('active');
+	  $(this).addClass('nav-tab-active');
+	  // Set return tab url
+	  setRedirectURL(id);
+	}));
 
-        /**
-         * Set redirect url into form's input:hidden
-         * Note: Using hardcoded plugin option page slug
-         * @param url String
-         */
-        function setRedirectURL(url) {
-            var split = $input.val().split('?', 1);
-            //Update the tab id in last while keeping base url same
-            $input.val(split[0] + '?page=asga_options_page#top#' + url);
-        }
-    });
+	/**
+	 * Set redirect url into form's input:hidden
+	 * Note: Using hardcoded plugin option page slug
+	 * @param url String
+	 */
+	function setRedirectURL(url) {
+	  var split = $input.val().split('?', 1);
+	  //Update the tab id in last while keeping base url same
+	  $input.val(split[0] + '?page=asga_options_page#top#' + url);
+	}
+  });
 })(window, jQuery);
